f32e269161fb679bf19fd7e7c6436d4c65eb54c4
Claude-Zq <1574089391@qq.com>
2022-07-14 10:04:42 +0800

    Add Function updateMotto

--- wx/miniprogram/pages/index/index.ts
+++ wx/miniprogram/pages/index/index.ts
@@ -8,14 +8,15 @@ Page({
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    canIUseGetUserProfile: true,
+    canIUseOpenData: false// 如需尝试获取用户信息可改为false
   },
   // 事件处理函数
   bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs',
-    })
+    // wx.navigateTo({
+    //   url: '../logs/logs',
+    // })
+    console.log('tapped')
   },
   onLoad() {
     // @ts-ignore
@@ -24,7 +25,11 @@ Page({
         canIUseGetUserProfile: true
       })
     }
+   
+    this.updateMotto()
   },
+
+
   getUserProfile() {
     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
     wx.getUserProfile({
@@ -45,5 +50,28 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
-  }
+  },
+  updateMotto(){
+    let shouldStop = false
+    setTimeout(()=>{
+      shouldStop = true
+    },10000)
+
+    let count = 0
+
+    const update = () =>{
+      count++
+    if (!shouldStop){
+      this.setData({
+        motto: `Update count: ${count}`
+      },()=>{
+        update()
+      })
+    }
+    }
+
+    update()
+
+    
+  },
 })
