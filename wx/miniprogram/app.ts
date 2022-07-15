fe38c9bc0cd218e4f89d88e792809e85c9f1eb3d
Claude-Zq <1574089391@qq.com>
2022-07-15 12:26:31 +0800

    使用async/await函数改写接口

--- wx/miniprogram/app.ts
+++ wx/miniprogram/app.ts
@@ -1,7 +1,17 @@
+import { getSetting, getUserProfile } from "./utils/util"
+
+let resolveUserInfo: (value: WechatMiniprogram.UserInfo | PromiseLike<WechatMiniprogram.UserInfo>) => void
+let rejectUserInfo: (reason?: any) => void
+
 // app.ts
 App<IAppOption>({
-  globalData: {},
-  onLaunch() {
+  globalData: {
+    userInfo:new Promise((resolve,reject)=>{
+        resolveUserInfo = resolve
+        rejectUserInfo = reject
+    })
+  },
+  async onLaunch() {
     // 展示本地存储能力
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
@@ -14,5 +24,20 @@ App<IAppOption>({
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       },
     })
+
+    //获取用户信息
+    try{
+      const setting = await getSetting()
+      if (setting.authSetting['scope.userInfo']){
+        const userInfoRes = await getUserProfile()
+        //首页通知我获得了用户信息
+        resolveUserInfo(userInfoRes.userInfo)
+      }
+    }catch(err){
+      rejectUserInfo(err)
+    }
   },
+  resolveUserInfo(userInfo: WechatMiniprogram.UserInfo) {
+    resolveUserInfo(userInfo)
+  }
 })
--- wx/miniprogram/pages/index/index.ts
+++ wx/miniprogram/pages/index/index.ts
@@ -13,33 +13,30 @@ Page({
   },
   // 事件处理函数
   bindViewTap() {
-    // wx.navigateTo({
-    //   url: '../logs/logs',
-    // })
-    console.log('tapped')
+    wx.navigateTo({
+      url: '../logs/logs',
+    })
   },
-  onLoad() {
-    // @ts-ignore
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
-   
-    this.updateMotto()
-  },
-
-
+  async onLoad() {
+    const userInfo = await app.globalData.userInfo
+    this.setData({
+      userInfo:userInfo,
+      hasUserInfo:true,
+    })
+  }
+  ,
   getUserProfile() {
     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
     wx.getUserProfile({
       desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
       success: (res) => {
+        const userInfo =  res.userInfo
         console.log(res)
         this.setData({
           userInfo: res.userInfo,
           hasUserInfo: true
-        })
+        })  
+        app.resolveUserInfo(userInfo)
       }
     })
   },
@@ -50,28 +47,5 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
-  },
-  updateMotto(){
-    let shouldStop = false
-    setTimeout(()=>{
-      shouldStop = true
-    },10000)
-
-    let count = 0
-
-    const update = () =>{
-      count++
-    if (!shouldStop){
-      this.setData({
-        motto: `Update count: ${count}`
-      },()=>{
-        update()
-      })
-    }
-    }
-
-    update()
-
-    
   },
 })
