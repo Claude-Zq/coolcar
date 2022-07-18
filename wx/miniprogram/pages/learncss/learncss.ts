7c94038124299f71e46b7d5cf3273a07597369f7
Claude-Zq <1574089391@qq.com>
2022-07-18 17:06:24 +0800

    TEST A PAGE TO LEARN CSS

---
+++ wx/miniprogram/pages/learncss/learncss.ts
@@ -0,0 +1,25 @@
+// pages/learncss/learncss.ts
+Page({
+  data:{
+    show:false,
+    hobbies:["摸鱼","撸铁","跑步"],
+    
+    emps:[
+      {
+        name:'john',
+        id: 1,
+
+      },
+      {
+        name:'mary',
+        id : 2
+      },
+      {
+        name :' tom',
+        id : 3
+      }
+
+    ]
+  },
+
+})
