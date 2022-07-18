ea644c3423d4b16ea7e55b168722c6afed700162
Claude-Zq <1574089391@qq.com>
2022-07-18 20:48:22 +0800

    完成了注册页的功能

--- wx/miniprogram/pages/register/register.ts
+++ wx/miniprogram/pages/register/register.ts
@@ -1,7 +1,13 @@
 Page({
 
   data:{
-    licImgURL : undefined as string| undefined,
+    licNo: '',
+    name: '',
+    genderIndex: 0,
+    genders: ['未知', '男', '女','其他'],
+    birthDate: '1990-01-01',
+    licImgURL: '',
+    state:'UNSUBMITTED' as 'UNSUBMITTED' | 'PENDING' | 'VERIFIED',
   },
 
   onUploadLic(){
@@ -11,8 +17,55 @@ Page({
           this.setData({
             licImgURL:res.tempFiles[0].tempFilePath
           })
+
+          //TODO: upload image
+          setTimeout(()=>{
+            this.setData({
+              licNo:'3252452345',
+              name:'张三',
+              genderIndex: 1,
+              birthDate:'2001-10-10',
+            })
+          },1000)
         }
       }
     })
   },
+
+
+  onGenderChange(e: any) {
+    this.setData({
+        genderIndex: parseInt(e.detail.value),
+    })
+  },
+  onBirthDateChange(e: any) {
+    this.setData({
+        birthDate: e.detail.value,
+    })
+  },
+  onSubmit() {
+    //TODO : submit the form to server
+    this.setData({
+        state:'PENDING',
+    })
+    setTimeout(()=>{
+      this.onLicVerified()
+    },3000)
+  },
+  onResubmit() {
+     //TODO : Resubmit the form to server
+    this.setData({
+      state:'UNSUBMITTED',
+      licImgURL:''
+    })
+  },
+  onLicVerified() {
+    this.setData({
+      state:'VERIFIED',
+    })
+    wx.redirectTo({
+      url:'/pages/lock/lock',
+    })
+  }
+
 })
