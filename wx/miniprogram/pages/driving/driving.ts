ac877dea9a97143c14618c32e0f7a36654c27551
Claude-Zq <1574089391@qq.com>
2022-07-19 13:08:46 +0800

    添加了实时更新位置的功能

--- wx/miniprogram/pages/driving/driving.ts
+++ wx/miniprogram/pages/driving/driving.ts
@@ -1,5 +1,54 @@
 const updateIntervalSec = 5
+const initialLat = 29.531873
+const initialLng = 106.607808
+
 
 Page({
+  timer: undefined as number|undefined,
+  tripID: '',
+
+  data: {
+      location: {
+          latitude: initialLat,
+          longitude: initialLng,
+      },
+      scale: 12,
+      elapsed: '00:00:00',
+      fee: '0.00',
+      markers: [
+          {
+              iconPath: "/resources/car.jpeg",
+              id: 0,
+              latitude: initialLat,
+              longitude: initialLng,
+              width: 20,
+              height: 20,
+          },
+      ],
+  },
+
+  onLoad(){
+    this.setupLocationUpdator()
+  },
+
+  onUnload(){
+    wx.stopLocationUpdate()
+  },
+
+
+  setupLocationUpdator(){
+    wx.startLocationUpdate({
+      fail:console.error,
+    })
+    wx.onLocationChange(loc=>{
+      console.log('location : ',loc)
+      this.setData({
+        location:{
+          latitude:loc.latitude,
+          longitude:loc.longitude,
+        },
+      })
+    })
+  },
   
 })
