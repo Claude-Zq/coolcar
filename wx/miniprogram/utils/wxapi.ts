8dea52f70bcb8c3e6cde6fa33852c369701f2f76
Claude-Zq <1574089391@qq.com>
2022-07-19 10:13:40 +0800

    调用getUserInfo后添加默认头像

--- wx/miniprogram/utils/wxapi.ts
+++ wx/miniprogram/utils/wxapi.ts
@@ -10,7 +10,18 @@ export function getSetting(): Promise<WechatMiniprogram.GetSettingSuccessCallbac
 export function getUserInfo(): Promise<WechatMiniprogram.GetUserInfoSuccessCallbackResult> {
   return new Promise((resolve, reject) => {
     wx.getUserInfo({
-      success: resolve,
+      success: res=>{
+        res.userInfo={
+          avatarUrl: '/resources/man.jpg', 
+          city: '重庆',
+          country: '中国',  
+          gender: 1,
+          language: "zh_CN",
+          nickName: '测试号',
+          province: '重庆',
+        }
+        return resolve
+      },
       fail: reject,
     })
   })
