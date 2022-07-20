aa2abe100e4c642258fc1905e602280197a64120
Claude-Zq <1574089391@qq.com>
2022-07-20 14:16:14 +0800

    完成了垂直导航功能

--- wx/miniprogram/pages/mytrips/mytrips.ts
+++ wx/miniprogram/pages/mytrips/mytrips.ts
@@ -1,174 +1,195 @@
 import { routing } from "../../utils/routing"
 
 interface Trip {
-  id: string
-  shortId: string
-  start: string
-  end: string
-  duration: string
-  fee: string
-  distance: string
-  status: string
-  inProgress: boolean
+    id: string
+    start: string
+    end: string
+    duration: string
+    fee: string
+    distance: string
+    status: string
 }
 
 interface MainItem {
-  id: string
-  navId: string
-  navScrollId: string
-  data: Trip
+    id: string
+    navId: string
+    navScrollId: string
+    data: Trip
 }
 
 interface NavItem {
-  id: string
-  mainId: string
-  label: string
+    id: string
+    mainId: string
+    label: string
 }
 
 interface MainItemQueryResult {
-  id: string
-  top: number
-  dataset: {
-      navId: string
-      navScrollId: string
-  }
+    id: string
+    top: number
+    dataset: {
+        navId: string
+        navScrollId: string
+    }
 }
 
 Page({
-  scrollStates: {
-      mainItems: [] as MainItemQueryResult[],
-  },
+    scrollStates: {
+        mainItems: [] as MainItemQueryResult[],
+    },
 
-  layoutResolver: undefined as (()=>void)|undefined,
+    data: {
+        promotionItems: [
+            {
+                img: 'https://i0.hdslb.com/bfs/banner/93cdd2d9c1a6a2c02b407090164f8fdc99658f1a.jpg@976w_550h_1c.webp',
+                promotionID: 1,
+            },            
+            {
+                img: 'https://i0.hdslb.com/bfs/banner/0cdf10d9501d28156b479cfe24c634dd8eb222d1.jpg@976w_550h_1c.webp'
+            },
+            {
+                img: 'https://i0.hdslb.com/bfs/banner/4115a245830d32634b9fe5127749ec06af450e38.png@976w_550h_1c.webp',
+                promotionID: 3,
+            },
+            {
+                img: 'https://i0.hdslb.com/bfs/banner/3504a32e6eb3588dcca1b07585160dfcee78af45.png@976w_550h_1c.webp',
+                promotionID: 4,
+            },
+        ],
+        avatarURL: '',
+        tripsHeight: 0,
+        navCount: 0,
+        mainItems: [] as MainItem[],
+        mainScroll: '',
+        navItems: [] as NavItem[],
+        navSel: '',
+        navScroll: '',
+    },
 
-  data: {
-      promotionItems: [
-          {
-              img: 'https://i0.hdslb.com/bfs/banner/de779b9b2a10a59c4c3e21754d59926024012b33.jpg@976w_550h_1c.webp',
-              promotionID: 1,
-          },            
-          {
-              img: 'https://w.wallhaven.cc/full/m9/wallhaven-m9jqz9.jpg',
-              promotionID: 2,
-          },
-          {
-              img: 'https://i0.hdslb.com/bfs/banner/9fce93d4d936eaa5847a9014436a5be6548093d7.jpg@976w_550h_1c.webp',
-              promotionID: 3,
-          },
-          {
-              img: 'https://i0.hdslb.com/bfs/banner/b8089866e3e7ed8073bab69b533c69483ef8dd71.png@976w_550h_1c.webp',
-              promotionID: 4,
-          },
-          {
-            img: 'https://i0.hdslb.com/bfs/banner/ba26f24c8972791bf8db14015bc7881e61cce97d.png@976w_550h_1c.webp',
-            promotionID: 5,
-        },   
-      ],
-  
-      avatarURL: '',
-      tripsHeight: 0,
-      navCount: 0,
-      mainItems: [] as MainItem[],
-      mainScroll: '',
-      navItems: [] as NavItem[],
-      navSel: '',
-      navScroll: '',
-  },
+    async onLoad() {
+        this.populateTrips()
+        const userInfo = await getApp<IAppOption>().globalData.userInfo
+        this.setData({
+            avatarURL: userInfo.avatarUrl,
+        })
+    },
 
-  async onLoad() {
-    const userInfo = await getApp<IAppOption>().globalData.userInfo
-    this.setData({
-      avatarURL:userInfo.avatarUrl,
-    })
-  },
+    onReady() {
+        wx.createSelectorQuery().select('#heading')
+            .boundingClientRect(rect => {
+                const height = wx.getSystemInfoSync().windowHeight - rect.height
+                this.setData({
+                    tripsHeight: height,
+                    navCount: Math.round(height/50),
+                })
+            }).exec()
+    },
 
-  onShow() {
-    
-  },
+    populateTrips() {
+        const mainItems: MainItem[] = []
+        const navItems: NavItem[] = []
+        let navSel = ''
+        let prevNav = ''
+        for (let i = 0; i < 100; i++) {
+            const mainId = 'main-' + i
+            const navId = 'nav-' + i
+            const tripId = (10001 + i).toString()
+            if (!prevNav) {
+                prevNav = navId
+            }
+            mainItems.push({
+                id: mainId,
+                navId: navId,
+                navScrollId: prevNav,
+                data: {
+                    id: tripId,
+                    start: '东方明珠',
+                    end: '迪士尼',
+                    distance: '27.0公里',
+                    duration: '0时44分',
+                    fee: '128.00元',
+                    status: '已完成',
+                },
+            })
+            navItems.push({
+                id: navId,
+                mainId: mainId,
+                label: tripId,
+            })
+            if (i === 0) {
+                navSel = navId
+            }
+            prevNav = navId
+        }
+        this.setData({
+            mainItems,
+            navItems,
+            navSel,
+        }, () => {
+            this.prepareScrollStates()
+        })
+    },
 
-  onReady() {
-      wx.createSelectorQuery().select('#heading')
-          .boundingClientRect(rect => {
-              const height = wx.getSystemInfoSync().windowHeight - rect.height
-              this.setData({
-                  tripsHeight: height,
-                  navCount: Math.round(height/50),
-              }, () => {
-                  if (this.layoutResolver) {
-                      this.layoutResolver()
-                  }
-              })
-          }).exec()
-  },
+    prepareScrollStates() {
+        wx.createSelectorQuery().selectAll('.main-item')
+            .fields({
+                id: true,
+                dataset: true,
+                rect: true,
+            }).exec(res => {
+                this.scrollStates.mainItems = res[0]
+            })
+    },
 
-  populateTrips() {
+    onPromotionItemTap(e: any) {
+        const promotionID:number = e.currentTarget.dataset.promotionId
+        if (promotionID) {
+            console.log('claiming promotion', promotionID)
+        }
+    },
 
-  },
+    onGetUserInfo(e: any) {
+        const userInfo: WechatMiniprogram.UserInfo = e.detail.userInfo
+        if (userInfo) {
+            getApp<IAppOption>().resolveUserInfo(userInfo)
+            this.setData({
+                avatarURL: userInfo.avatarUrl,
+            })
+        }
+    },
 
-  prepareScrollStates() {
-      wx.createSelectorQuery().selectAll('.main-item')
-          .fields({
-              id: true,
-              dataset: true,
-              rect: true,
-          }).exec(res => {
-              this.scrollStates.mainItems = res[0]
-          })
-  },
+    onRegisterTap() {
+        wx.navigateTo({
+            url: routing.register(),
+        })
+    },
 
-  onPromotionItemTap(e: any) {
-      const promotionID:number = e.currentTarget.dataset.promotionId
-      if (promotionID) {
-          console.log('claiming promotion', promotionID)
-      }
-  },
+    onNavItemTap(e: any) {
+        const mainId: string = e.currentTarget?.dataset?.mainId
+        const navId: string = e.currentTarget?.id
+        if (mainId && navId) {
+            this.setData({
+                mainScroll: mainId,
+                navSel: navId,
+            })
+        }
+    },
 
-  onGetUserInfo(e: any) {
-      const userInfo: WechatMiniprogram.UserInfo = e.detail.userInfo
-      if (userInfo) {
-          getApp<IAppOption>().resolveUserInfo(userInfo)
-          this.setData({
-              avatarURL: userInfo.avatarUrl,
-          })
-      }
-  },
+    onMainScroll(e: any) {
+        console.log(e)
+        const top: number = e.currentTarget?.offsetTop + e.detail?.scrollTop
+        if (top === undefined) {
+            return
+        }
 
-  onRegisterTap() {
-    wx.navigateTo({
-      url: routing.register(),
-    })
-     
-  },
+        const selItem = this.scrollStates.mainItems.find(
+            v => v.top >= top)
+        if (!selItem) {
+            return
+        }
 
-  onNavItemTap(e: any) {
-      const mainId: string = e.currentTarget?.dataset?.mainId
-      const navId: string = e.currentTarget?.id
-      if (mainId && navId) {
-          this.setData({
-              mainScroll: mainId,
-              navSel: navId,
-          })
-      }
-  },
-
-  onMainScroll(e: any) {
-      const top: number = e.currentTarget?.offsetTop + e.detail?.scrollTop
-      if (top === undefined) {
-          return
-      }
-
-      const selItem = this.scrollStates.mainItems.find(
-          v => v.top >= top)
-      if (!selItem) {
-          return
-      }
-
-      this.setData({
-          navSel: selItem.dataset.navId,
-          navScroll: selItem.dataset.navScrollId,
-      })
-  },
-
-  onMianItemTap(e: any) {
-  }
+        this.setData({
+            navSel: selItem.dataset.navId,
+            navScroll: selItem.dataset.navScrollId,
+        })
+    }
 })
