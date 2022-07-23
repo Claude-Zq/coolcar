d1cccefe70da084b3a0097683fa2126585b6ecde
Claude-Zq <1574089391@qq.com>
2022-07-23 13:52:22 +0800

    将grpc-gataway放回的下划线风格的变量转为驼峰风格

--- wx/miniprogram/app.ts
+++ wx/miniprogram/app.ts
@@ -1,5 +1,7 @@
 import { getSetting, getUserInfo } from "./utils/wxapi"
 import {IAppOption} from "./appoption"
+import camelcaseKeys = require("camelcase-keys")
+import { coolcar } from "./service/proto_gen/trip_pb"
 let resolveUserInfo: (value: WechatMiniprogram.UserInfo | PromiseLike<WechatMiniprogram.UserInfo>) => void
 let rejectUserInfo: (reason?: any) => void
 
@@ -15,7 +17,14 @@ App<IAppOption>({
     wx.request({
       url:'http://localhost:8080/trip/trip123',
       method:'GET',
-      success:console.log,
+      success: res => {
+        const getTripRes = coolcar.GetTripResponse.fromObject(
+          camelcaseKeys(res.data as object, {
+            deep: true,
+          }))
+        console.log(getTripRes)
+        console.log('status is', coolcar.TripStatus[getTripRes.trip?.status!])
+      },
       fail:console.error
     })
 
