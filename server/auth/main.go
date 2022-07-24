301aea268129828ba1412ed19985e588d2741033
Claude-Zq <1574089391@qq.com>
2022-07-24 10:08:45 +0800

    添加openId 检索器

--- server/auth/main.go
+++ server/auth/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	authpb "coolcar/auth/api/gen/v1"
 	"coolcar/auth/auth"
+	"coolcar/auth/wechat"
 	"log"
 	"net"
 
@@ -23,6 +24,10 @@ func main() {
 
 	s := grpc.NewServer()
 	authpb.RegisterAuthServiceServer(s, &auth.Service{
+		OpenIDResolver: &wechat.Service{
+			AppID:     "wx361a81db28527b67",
+			AppSecret: "3b9f1dffe511621d84ee4c9b71fa066d",
+		},
 		Logger: logger,
 	})
 
--- server/go.mod
+++ server/go.mod
@@ -11,6 +11,7 @@ require (
 
 require (
 	github.com/google/go-cmp v0.5.8 // indirect
+	github.com/medivhzhan/weapp/v2 v2.5.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.21.0 // indirect
